c93d79089123477611ed83d28392653955bdd4b0
ArtHoly26 <112950470+ArtHoly26@users.noreply.github.com>
2023-10-02 20:03:15 +0200

    Add project files.

---
+++ HomeWork/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    class AgeValidationAttribute : Attribute
+    {
+        public int Age { get; }
+        public AgeValidationAttribute() { }
+        public AgeValidationAttribute(int age) => Age = age;
+    }
+
+    [AgeValidation(18)]
+    public class Person
+    {
+        public string Name { get; }
+        public int Age { get; set; }
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
---
+++ HomeWork/Program.cs
@@ -0,0 +1,25 @@
+﻿using HomeWork;
+Person tom = new Person("Tom", 25);
+Person bob = new Person("Bob", 16);
+bool tomIsValid = ValidateUser(tom);    
+bool bobIsValid = ValidateUser(bob);    
+
+Console.WriteLine($"Проверка Тома: {tomIsValid}");
+Console.WriteLine($"Проверка Боба: {bobIsValid}");
+
+bool ValidateUser(Person person)
+{
+Type type = typeof(Person);
+object[] attributes = type.GetCustomAttributes(false);
+
+foreach (Attribute attr in attributes)
+{
+
+if (attr is AgeValidationAttribute ageAttribute)
+
+return person.Age >= ageAttribute.Age;
+}
+return true;
+}
+
+
